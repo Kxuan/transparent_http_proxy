21d950113d5e7b69d23efb13c2d4722156dc3908
kXuan <kxuanobj@gmail.com>
2016-09-29 16:33:23 +0800

    add http basic auth support

--- tcp.js
+++ tcp.js
@@ -21,7 +21,12 @@ const cfg = {
     },
     proxy: {
         host: "10.167.196.133",
-        port: 8080
+        port: 8080,
+        auth: {
+            method: 'None', //'Basic' or other value
+            user: 'user',
+            pass: 'pass'
+        }
     }
 };
 
@@ -34,12 +39,9 @@ var rl = null;
 
 var s = net.createServer(onNewClient);
 
-function Channel(c, proxy_host, proxy_port) {
-    var events={};
-    this.proxy = {
-        host: proxy_host,
-        port: proxy_port
-    };
+function Channel(c, proxy) {
+    var events = {};
+    this.proxy = proxy;
     this.sock_client = c;
 
     c.once('close', events.c_close = onpeer_closed.bind(this));
@@ -53,18 +55,35 @@ function Channel(c, proxy_host, proxy_port) {
         this.emit("close");
         cleanup_post.apply(this);
     }
+
     function cleanup_pre() {
         if (this.sock_proxy)
             this.sock_proxy.end();
         this.sock_client.end();
     }
+
     function cleanup_post() {
         delete this.sock_client;
         delete this.sock_proxy;
         this.removeAllListeners();
     }
 }
+Channel.encryptHelloMessage = function (target_host, target_port, proxy) {
+    switch (proxy.auth || proxy.auth.method) {
+        case 'Basic': {
+            let credential = proxy.user || "";
+            if (proxy.pass) credential += ":" + proxy.pass;
 
+            return util.format(
+                "CONNECT %s:%d\nProxy-Authorization: Basic %s\n\n",
+                target_host, target_port,
+                (new Buffer(credential)).toString("base64")
+            );
+        }
+        default:
+            return util.format("CONNECT %s:%d\n\n", target_host, target_port);
+    }
+};
 util.inherits(Channel, EventEmitter);
 Channel.prototype.state = 'wait';
 Channel.prototype.forward = function (host, port) {
@@ -75,7 +94,9 @@ Channel.prototype.forward = function (host, port) {
     var s = net.connect(this.proxy.port, this.proxy.host, onconnected.bind(this));
     s.once('data', ondata.bind(this));
     s.on('error', onerror.bind(this));
-    s.once('close', function(){c.end();});
+    s.once('close', function () {
+        c.end();
+    });
 
     this.state = 'connecting';
 
@@ -84,13 +105,12 @@ Channel.prototype.forward = function (host, port) {
         s.end();
         c.end();
     }
+
     /**
      * @this {Channel}
      */
     function onconnected() {
-        var req_body;
-        req_body = util.format("CONNECT %s:%d\n", host, port);
-        s.write(req_body);
+        s.write(Channel.encryptHelloMessage(host, port, this.proxy));
         this.state = 'request';
         this.emit('request');
     }
@@ -111,6 +131,7 @@ Channel.prototype.forward = function (host, port) {
         s.pipe(c);
         c.pipe(s);
     }
+
     this.sock_proxy = s;
 };
 
@@ -128,8 +149,8 @@ Channel.prototype.info = function () {
                 port: c.remotePort
             }
         },
-    }
-    if(s) {
+    };
+    if (s) {
         r["proxy"] = {
             local: {
                 addr: s.localAddress,
@@ -151,7 +172,7 @@ Channel.prototype.log = function (fmt) {
 };
 
 function onNewClient(c) {
-    var chan = new Channel(c, cfg.proxy.host, cfg.proxy.port);
+    var chan = new Channel(c, cfg.proxy);
     var l_addr = chan.info().client.remote;
     chan.name = util.format("%s:%d", l_addr.addr, l_addr.port);
     chan.id = clients_id++;
@@ -162,7 +183,7 @@ function onNewClient(c) {
     chan.on('request', () => chan.log("Requesting"));
     chan.on('fail', () => chan.log("Failed"));
     chan.on('connected', () => chan.log("Established"));
-    
+
     clients[chan.id] = chan;
     if (!cfg.target.host) {
         chan.log("Waiting forward");
@@ -179,97 +200,93 @@ function setupServer() {
         rl.on('line', onReadlineLine);
         rl.on('close', () => process.exit(0));
     }
-    s.listen(cfg.local.listen, () => writeLog("server ready") );
+    s.listen(cfg.local.listen, () => writeLog("server ready"));
     s.on('error', console.error.bind(console));
 }
 
 function onReadlineLine(line) {
     var args = line.split(/\s/);
     if (!args) return;
-    try{
-    switch(args[0]) {
-        case 'exit':
-        case 'quit':
-            process.exit(0);
-            break;
-        case 'list':
-        case 'ls':
-            console.log("id addr status");
-            console.log("----------------------------------------");
-            for(var id in clients) {
-                let client = clients[id];
-                if (!client) continue;
-                console.log("%d %s %s", id, client.name, client.state);
-            }
-            break;
-        case 'listen':
-        {
-            let port = args[1];
-            if (!port) {
-                console.log("listen <port>");
+    try {
+        switch (args[0]) {
+            case 'exit':
+            case 'quit':
+                process.exit(0);
                 break;
-            }
-            s.close();
-            cfg.local.listen = port;
-            s = net.createServer();
-            setupServer();
-        }
-            break;
-        case 'forward':
-        {
-            let name = args[1];
-            let host = args[2];
-            let port = args[3];
-            if (!name || !host || !port) {
-                console.log("forward <client_id> <host> <port>");
+            case 'list':
+            case 'ls':
+                console.log("id addr status");
+                console.log("----------------------------------------");
+                for (var id in clients) {
+                    let client = clients[id];
+                    if (!client) continue;
+                    console.log("%d %s %s", id, client.name, client.state);
+                }
                 break;
+            case 'listen': {
+                let port = args[1];
+                if (!port) {
+                    console.log("listen <port>");
+                    break;
+                }
+                s.close();
+                cfg.local.listen = port;
+                s = net.createServer();
+                setupServer();
             }
-            let client = clients[name];
-            if (!client) {
-                console.log("Client not found\n");
                 break;
+            case 'forward': {
+                let name = args[1];
+                let host = args[2];
+                let port = args[3];
+                if (!name || !host || !port) {
+                    console.log("forward <client_id> <host> <port>");
+                    break;
+                }
+                let client = clients[name];
+                if (!client) {
+                    console.log("Client not found\n");
+                    break;
+                }
+                console.log("forward %s to %s:%s", client.name, host, port);
+                client.forward(host, port);
             }
-            console.log("forward %s to %s:%s", client.name, host, port);
-            client.forward(host, port);
-        }
-            break;
-        case 'target':
-        {
-            let host = args[1];
-            let port = args[2];
-            if (host && !port) {
-                console.log("target <host> <port>");
                 break;
+            case 'target': {
+                let host = args[1];
+                let port = args[2];
+                if (host && !port) {
+                    console.log("target <host> <port>");
+                    break;
+                }
+                if (!host) {
+                    cfg.target.host = null;
+                    cfg.target.port = null;
+                    console.log("Default forward target is clear.\nClient will not be forward automaticly");
+                } else {
+                    cfg.target.host = host;
+                    cfg.target.port = port;
+                    console.log("Default forward target set.\nClient will be forward automaticly");
+                }
             }
-            if (!host) {
-                cfg.target.host = null;
-                cfg.target.port = null;
-                console.log("Default forward target is clear.\nClient will not be forward automaticly");
-            } else {
-                cfg.target.host = host;
-                cfg.target.port = port;
-                console.log("Default forward target set.\nClient will be forward automaticly");
+                break;
+            case 'config': {
+                let output = args[1];
+                if (!output) {
+                    console.log(cfg);
+                } else {
+                    fs.writeFileSync(output, JSON.stringify(cfg));
+                }
             }
+                break;
+            case '?':
+            case 'help':
+                console.log("Help\n======\nexit    - exit\nquit    - alias of `exit`\nlist    - list all clients\nls      - alias of `list`\nlisten  - replace the listen port and keep clients connected.\nforward - forward a client. (only wait state)\ntarget  - set auto-forward target or disable auto forward\nconfig  - print config to screen or write to a file\n");
+                break;
+            default:
+                console.log("Unknow command '%s'.", args[0]);
         }
-            break;
-        case 'config':
-        {
-            let output = args[1];
-            if (!output) {
-                console.log(cfg);
-            } else {
-                fs.writeFileSync(output, JSON.stringify(cfg));
-            }
-        }
-            break;
-        case '?':
-        case 'help':
-            console.log("Help\n======\nexit    - exit\nquit    - alias of `exit`\nlist    - list all clients\nls      - alias of `list`\nlisten  - replace the listen port and keep clients connected.\nforward - forward a client. (only wait state)\ntarget  - set auto-forward target or disable auto forward\nconfig  - print config to screen or write to a file\n");
-            break;
-        default:
-            console.log("Unknow command '%s'.", args[0]);
-    }
-    }catch(ex){
+    } catch (ex) {
         console.error(ex);
     }
     rl.prompt();
@@ -286,16 +303,19 @@ function writeLog(fmt) {
 
 switch (process.argv.length) {
     case 4:
-    // node tcp.js <host> <port>
+        // node tcp.js <host> <port>
         cfg.target.host = process.argv[2];
         cfg.target.port = parseInt(process.argv[3]);
+        break;
     case 3:
-    // node tcp.js <config.json>
+        // node tcp.js <config.json>
         let outer_cfg = JSON.parse(fs.readFileSync(process.argv[2]));
+
+        //TODO merge config object
         Object.assign(cfg, outer_cfg);
         break;
     case 2:
-    // node tcp.js
+        // node tcp.js
         break;
     default:
         console.error("Hey, check your arguments.");
